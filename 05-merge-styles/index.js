475ff4aa4a8a10ceacd76c35f38b02d055c2761c
Vitali86 <vitali86@tut.by>
2024-01-22 08:56:21 +0300

    fix: implement check is a file

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,7 @@
 // Possible steps to complete the task:
 
 const { readdir, readFile, createWriteStream } = require('fs');
+const { stat } = require('fs/promises');
 const { join, parse } = require('path');
 
 // 1. Import all required modules.
@@ -14,14 +15,14 @@ const stylesDir = join(__dirname, 'styles');
 
 const singleFile = join(__dirname, 'project-dist', 'bundle.css');
 
-readdir(stylesDir, (err, files) => {
+readdir(stylesDir, async (err, files) => {
   if (err) throw err;
 
   // console.log(files);
-  const bundle = createWriteStream(singleFile);
+  const bundle = await createWriteStream(singleFile);
   for (const file of files) {
-    if (parse(file).ext === '.css') {
-      readFile(join(stylesDir, file), (err, data) => {
+    if (parse(file).ext === '.css' && (await stat(join(stylesDir, file))).isFile()) {
+      await readFile(join(stylesDir, file), (err, data) => {
         if (err) throw err;
         // console.log(data);
         bundle.write(data);
